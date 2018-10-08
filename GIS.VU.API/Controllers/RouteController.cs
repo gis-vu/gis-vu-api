e2f4be403cffbfd92ad1318b4abaee558b8ca677
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-10-08 08:22:55 +0300

    Initial

---
+++ GIS.VU.API/Controllers/RouteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GIS.VU.API.Controllers
+{
+    
+    [ApiController]
+    public class RouteController : ControllerBase
+    {
+        [Route("")]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        [Route("api/[controller]")]
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
