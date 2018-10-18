693606039fdf77ccb9dbe88dd41d1a5763fc0b95
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-10-18 14:27:42 +0300

    Resharper

--- GIS.VU.API/RouteSearchEngine.cs
+++ GIS.VU.API/RouteSearchEngine.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using BAMCIS.GeoJSON;
 using GIS.VU.API.DTOs;
-using Microsoft.AspNetCore.Mvc;
 
 namespace GIS.VU.API
 {
     public class RouteSearchEngine
     {
-        private RouteFeature[] routeFeatures;
+        private readonly RouteFeature[] _routeFeatures;
 
         public RouteSearchEngine(GeoJsonFileReader fileReader, string path)
         {
-            routeFeatures = fileReader.Read(path).ToArray();
+            _routeFeatures = fileReader.Read(path).ToArray();
         }
 
         public RouteSearchResponse FindRoute(RouteSearchRequest request)
@@ -23,12 +21,12 @@ namespace GIS.VU.API
             var endFeature = FindClosetFeature(request.End);
 
 
-            Graph g1 = new Graph(routeFeatures);
+            var g1 = new Graph(_routeFeatures);
             var path = g1.shortest_path(startFeature, endFeature);
             var route1 = PathToRoute(path);
 
-            if(path.Count == 2)
-                return new RouteSearchResponse(new[] { route1 });
+            if (path.Count == 2)
+                return new RouteSearchResponse(new[] {route1});
 
             var routeFeature = path.Skip(path.Count / 2).First();
 
@@ -37,40 +35,29 @@ namespace GIS.VU.API
             routeFeature.Length = double.MaxValue / 2;
             var route2 = PathToRoute(g1.shortest_path(startFeature, endFeature));
             routeFeature.Length = oldValue;
-            //var routeFeatureClone = new RouteFeature()
-            //{
-            //    Feature = routeFeature.Feature,
-            //    Neighbours = routeFeature.Neighbours,
-            //    Length = double.MaxValue / 2
-            //};
 
-            //var routeFeaturesClone = (RouteFeature[])routeFeatures.Clone();
-            //var index = Array.FindIndex(routeFeaturesClone, e => e == routeFeature);
-
-            //routeFeaturesClone[index] = routeFeatureClone;
-
-            //Graph g2 = new Graph(routeFeaturesClone);
-            //var route2 = PathToRoute(g2.shortest_path(startFeature, endFeature));
-
-            return new RouteSearchResponse(new[] { route1, route2 });
+            return new RouteSearchResponse(new[] {route1, route2});
         }
 
         private Route PathToRoute(List<RouteFeature> path)
         {
-            return new Route(){
-                Data = new RouteData()
+            return new Route
+            {
+                Data = new RouteData
                 {
                     Type = "LineString",
-                    Coordinates = SorthPath(path.Select(x => ((LineString)x.Feature.Geometry).Coordinates.Select(y => new[] { y.Longitude, y.Latitude }).ToArray()))
+                    Coordinates = SorthPath(path.Select(x =>
+                        ((LineString) x.Feature.Geometry).Coordinates.Select(y => new[] {y.Longitude, y.Latitude})
+                        .ToArray()).ToArray())
                 },
-                Info = new RouteInfo()
+                Info = new RouteInfo
                 {
-                    Length = Math.Round(path.Sum(x => x.Length),2)
+                    Length = Math.Round(path.Sum(x => x.Length), 2)
                 }
             };
         }
 
-        private double[][] SorthPath(IEnumerable<double[][]> path)
+        private double[][] SorthPath(IList<double[][]> path)
         {
             var coordinates = new List<double[]>();
             var last = path.First().Last();
@@ -78,25 +65,20 @@ namespace GIS.VU.API
             coordinates.AddRange(path.First());
 
             foreach (var f in path.Skip(1))
-            {
-                if(AreClose(last, f.First()))
+                if (AreClose(last, f.First()))
                 {
                     coordinates.AddRange(f);
                     last = f.Last();
                 }
-                else if(AreClose(last, f.Last()))//apversti
+                else if (AreClose(last, f.Last())) //apversti
                 {
                     coordinates.AddRange(f.Reverse());
                     last = f.First();
                 }
                 else //jei reikia deti i prieki
                 {
-                    if (AreClose(coordinates.First(), f.Last()))
-                        coordinates = f.Concat(coordinates.ToArray()).ToList();
-                    else
-                        coordinates = f.Reverse().Concat(coordinates.ToArray()).ToList();
+                    coordinates = AreClose(coordinates.First(), f.Last()) ? f.Concat(coordinates.ToArray()).ToList() : f.Reverse().Concat(coordinates.ToArray()).ToList();
                 }
-            }
 
             return coordinates.ToArray();
         }
@@ -111,10 +93,10 @@ namespace GIS.VU.API
 
         private RouteFeature FindClosetFeature(Coordinate coordinate)
         {
-            var closet = routeFeatures.First();
-            double dist = CalcualteDistanceToFeature(closet, coordinate);
+            var closet = _routeFeatures.First();
+            var dist = CalcualteDistanceToFeature(closet, coordinate);
 
-            foreach (var f in routeFeatures.Skip(1))
+            foreach (var f in _routeFeatures.Skip(1))
             {
                 var newDistance = CalcualteDistanceToFeature(f, coordinate);
 
@@ -130,13 +112,13 @@ namespace GIS.VU.API
 
         private double CalcualteDistanceToFeature(RouteFeature feature, Coordinate coordinate)
         {
-            Tuple<Position, Position>[] lineSegments = SplitFeatureIntoLineSegments(feature);
+            var lineSegments = SplitFeatureIntoLineSegments(feature);
 
-            double distance = GetDistance(lineSegments.First(), coordinate);
+            var distance = GetDistance(lineSegments.First(), coordinate);
 
             foreach (var c in lineSegments.Skip(1))
             {
-                var newDistance = GetDistance(c,coordinate);
+                var newDistance = GetDistance(c, coordinate);
 
                 if (newDistance < distance)
                     distance = newDistance;
@@ -147,7 +129,7 @@ namespace GIS.VU.API
 
         private Tuple<Position, Position>[] SplitFeatureIntoLineSegments(RouteFeature feature)
         {
-            var coords = ((LineString)feature.Feature.Geometry).Coordinates;
+            var coords = ((LineString) feature.Feature.Geometry).Coordinates;
             var lineSegments = new List<Tuple<Position, Position>>();
 
             var lastPosition = coords.First();
@@ -163,7 +145,12 @@ namespace GIS.VU.API
 
         private double GetDistance(Tuple<Position, Position> lineSegment, Coordinate point)
         {
-            double x = point.Lat, y= point.Lng, x1=lineSegment.Item1.Latitude, y1= lineSegment.Item1.Longitude, x2= lineSegment.Item2.Latitude, y2= lineSegment.Item2.Longitude;
+            double x = point.Lat,
+                y = point.Lng,
+                x1 = lineSegment.Item1.Latitude,
+                y1 = lineSegment.Item1.Longitude,
+                x2 = lineSegment.Item2.Latitude,
+                y2 = lineSegment.Item2.Longitude;
 
             var A = x - x1;
             var B = y - y1;
@@ -198,11 +185,6 @@ namespace GIS.VU.API
             var dy = y - yy;
             return Math.Sqrt(dx * dx + dy * dy);
         }
-
-        //private double GetDistance(Position a, Coordinate b)
-        //{
-        //    return Math.Sqrt(Math.Pow(a.Latitude - b.Lat, 2) + Math.Pow(a.Longitude - b.Lng, 2));
-        //}
 
         private double GetDistance(double[] first, double[] second)
         {
