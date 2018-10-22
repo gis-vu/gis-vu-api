2f21c8b18c44427ff9a5a6557807a93b6301459e
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-10-22 11:38:57 +0300

    WIP forrest

--- GIS.VU.API/DTOs/SearchOptions.cs
+++ GIS.VU.API/DTOs/SearchOptions.cs
@@ -3,6 +3,7 @@
     public class SearchOptions
     {
         public PropertyImportance[] PropertyImportance { get; set; }
+        public PropertyValueImportance[] PropertyValueImportance { get; set; }
         public double TrackOverlapImportance { get; set; }
     }
 }
